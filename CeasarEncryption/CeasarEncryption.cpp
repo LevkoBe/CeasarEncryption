0708315bc8fc0413436663e3407ce4cd16cefef2
LevkoBe <lbeniakh@kse.org.ua>
2023-10-11 12:51:36 +0300

    Load-unload

--- CeasarEncryption/CeasarEncryption.cpp
+++ CeasarEncryption/CeasarEncryption.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-#include "Encrypt.h"
+#include <windows.h>
 
+typedef std::string(*EncryptFunction)(const std::string, int);
+typedef std::string(*DecryptFunction)(const std::string, int);
 
 int main()
 {
-	std::string str = "Hello, I'm encrypted text!";
-	std::string encr = encrypt(str, 13);
-	for (int i = 0; i < 10; i++)
-	{
-		std::string encrypted = encrypt(str, i);
-		std::cout << encrypted << std::endl;
-		std::cout << decrypt(encrypted, i) << std::endl;
-		std::cout << decrypt(encrypted, i + 1) << std::endl;
-		std::cout << std::endl;
-	}
-	return 0;
+    HINSTANCE handle = LoadLibrary(TEXT("CeasarLibrary.dll"));
+    if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Library not found" << std::endl;
+        return 1;
+    }
+
+    EncryptFunction encrypt = (EncryptFunction)GetProcAddress(handle, "encrypt");
+    DecryptFunction decrypt = (DecryptFunction)GetProcAddress(handle, "decrypt");
+
+    if (encrypt == nullptr || decrypt == nullptr)
+    {
+        std::cout << "Functions not found" << std::endl;
+        FreeLibrary(handle);
+        return 1;
+    }
+
+    std::string greetings = "Hello, I'm encrypted text!";
+    for (int i = 0; i < 10; i++)
+    {
+        std::string encrypted = encrypt(greetings, i);
+        std::cout << encrypted << std::endl;
+        std::cout << decrypt(encrypted, i) << std::endl;
+        std::cout << decrypt(encrypted, i + 1) << std::endl;
+        std::cout << std::endl;
+    }
+
+    FreeLibrary(handle);
+    return 0;
 }
